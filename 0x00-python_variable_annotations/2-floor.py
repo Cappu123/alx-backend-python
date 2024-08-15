bd8e9d002ba7c572180126c7e00da1de2c41b383
Cappu123 <cappyalemayehu12@gmail.com>
2024-08-15 21:29:08 +0000

    floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-'''Task 2's
-module'''
+'''Task 2's module.
+'''
 
-import math
 
-def floor(n: float) -> int:
-    '''takes in a float
-    returns the floor of it'''
-
-    return math.floor(n)
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
