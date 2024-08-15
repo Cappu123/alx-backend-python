c6bde0c03e1f9dc89c10d045201045676b93eec3
Cappu123 <cappyalemayehu12@gmail.com>
2024-08-15 21:30:57 +0000

    sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
-
+'''Task 5's module.
+'''
 from typing import List
 
-"""Task 5's module"""
 
 def sum_list(input_list: List[float]) -> float:
-    """
-    Calculates the sum of  a list of floats
-
-    Args: input_list(List[float]): A list of floats
-
-    Returns:
-        float: The sum of the input list
-    """
-    return sum(input_list)
+    '''Computes the sum of a list of floating-point numbers.
+    '''
+    return float(sum(input_list))
