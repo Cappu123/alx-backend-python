fb8d9ec992330cd68ef4c406993dd2a07e06fecc
Cappu123 <cappyalemayehu12@gmail.com>
2024-08-15 21:00:13 +0000

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Task 0's module
-"""
+'''Task 0's module.
+'''
+
 
 def add(a: float, b: float) -> float:
-    """annotated function to add a and b: floats
-    returning the sum as float"""
-    
+    '''Adds two floating-point numbers.
+    '''
     return a + b
