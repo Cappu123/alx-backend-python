eebf40a9975c07d9ddcfee97bd1879a93d536770
Cappu123 <cappyalemayehu12@gmail.com>
2024-08-15 09:44:01 +0000

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''module for
+Task 1'''
 
 def concat(str1: str, str2: str) -> str:
+    '''takes in two strings and
+    returns concatenated string'''
+
     return str1 + str2
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+'''Task 2's
+module'''
+
 import math
 
 def floor(n: float) -> int:
+    '''takes in a float
+    returns the floor of it'''
+
     return math.floor(n)
