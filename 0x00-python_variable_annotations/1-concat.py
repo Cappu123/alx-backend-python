0b44d959fc2b99c26f3bfc367b07997989a76c26
Cappu123 <cappyalemayehu12@gmail.com>
2024-08-15 21:01:37 +0000

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 '''module for
-Task 1'''
+Task 1
+'''
 
 def concat(str1: str, str2: str) -> str:
     '''takes in two strings and
-    returns concatenated string'''
-
+    returns concatenated string
+    '''
     return str1 + str2
