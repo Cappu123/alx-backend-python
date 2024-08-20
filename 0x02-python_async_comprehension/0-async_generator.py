c17037f6d744e4ded97025037e8b9d19cc24b364
Cappu123 <cappyalemayenu12@gmail.com>
2024-08-20 23:41:42 +0000

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,8 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
     """generates 10 random numbers sequence """
 
     for i in range(10):
